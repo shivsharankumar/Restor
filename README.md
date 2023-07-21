e448741daa03d26c1a8ec20418126aa525bd2d8e
Shiv Sharan Sharma <shiv@Shivs-MacBook-Pro.local>
2023-07-21 18:19:02 +0530

    Project Added

---
+++ README.md
@@ -0,0 +1,19 @@
+# restor
+restor is a website that lets you add and leave reviews for restaurants in India. You need to register to add restaurants or reviews. restor also
+uses Mapbox API to display clusters of restaurants throughout India.
+
+## Tools and Technologies Used
+   * Front-End
+     - HTML
+     - CSS
+     - Bootstrap
+     - JavaScript
+   * Back-End
+     - Node.js
+        - Express
+        - Mongoose ODM
+        - Passport.js for authentication
+     - MongoDB
+     - Cloudinary for image management
+     - Mapbox
+
---
+++ middleware.js
@@ -0,0 +1,55 @@
+const { restaurantSchema, reviewSchema } = require('./joiSchemas.js');
+const expressError = require('./utils/ExpressError');
+const Restaurant = require('./models/restaurant');
+const Review = require('./models/review');
+
+module.exports.isLoggedIn = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl
+        req.flash('error', 'You must be signed in first!');
+        return res.redirect('/login');
+    }
+    next();
+}
+
+
+module.exports.isAuthor = async (req, res, next) => {
+    const { id } = req.params;
+    const restaurant = await Restaurant.findById(id);
+    if (!restaurant.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/restaurants/${id}`);
+    }
+    next();
+}
+
+module.exports.validateRestaurant = (req, res, next) => {
+    //validation on the server side
+    const { error } = restaurantSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new expressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+module.exports.validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new expressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to do that!');
+        return res.redirect(`/restaurants/${id}`);
+    }
+    next();
+}
